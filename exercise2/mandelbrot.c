e05b15c852700e17efb90b77c9c8ca085fefef67
Andrea Esposito <aespositox@gmail.com>
2024-02-28 18:44:31 +0100

    Better MPI version

--- exercise2/mandelbrot.c
+++ exercise2/mandelbrot.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <complex.h>
 #include <math.h>
+#include <string.h>
+#include <sys/time.h>
 
 #include <mpi.h>
 #include <omp.h>
@@ -10,6 +12,11 @@
 
 #define mb_t unsigned short
 #define max_mb_t (int) pow(2, 8 * sizeof(mb_t)) - 1
+
+#define TAG_BCAST_DONE 1337
+#define TAG_TASK_READY 7
+#define TAG_TASK_ROW 8
+#define TAG_TASK_ROW_RESULT 9
 
 mb_t mandelbrot_func(double complex z, double complex c, int n, int Imax)
 {
@@ -45,107 +52,238 @@ mb_t* _mandelbrot_matrix(int nx, int ny, double xL, double yL, double xR, double
       y = yL + j * dy;
       c = x + I * y;
       matrix[j * nx + i] = mandelbrot_func(0 * I, c, 0, Imax);
-      // matrix[j * nx + j] = j * nx + j;
     }
   }
-  printf("M[0] = %d\n", matrix[0]);
-  printf("M[1] = %d\n", matrix[1]);
+
+  // int rank;
+  // char* partfile;
+  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  // partfile = (char*) malloc(sizeof(char) * 21);
+  // sprintf(partfile, "test%d.pgm", rank);
+
+  // write_pgm_image(matrix, Imax, nx, ny, partfile);
+  printf("matrix[0] = %d\n", matrix[0]);
+  printf("matrix[1] = %d\n", matrix[1]);
+
+  // free(partfile);
   return matrix;
 }
 
-mb_t* mandelbrot_matrix(int nx, int ny, double xL, double yL, double xR, double yR, int Imax)
+mb_t* _mandelbrot_matrix_row(int r, int nx, int ny, double xL, double yL, double xR, double yR, int Imax)
 {
+  double dx, dy, x, y;
+  double complex c;
+  mb_t* matrix;
+
+  printf("nx = %d, ny = %d, xL = %.5f, yL = %.5f, xR = %.5f, yR = %.5f, Imax = %d\n", nx, ny, xL, yL, xR, yR, Imax);
+
+  dx = (double) (xR - xL) / (double) (nx - 1);
+  printf("dx = %.2f\n", dx);
+  dy = (double) (yR - yL) / (double) (ny - 1);
+  printf("dy = %.2f\n", dy);
+
+  printf("nx * ny = %d\n", nx * ny);
+  matrix = (mb_t*) malloc(sizeof(mb_t) * nx);
+
+  // #pragma omp parallel for schedule(dynamic)
+  for (int i = 0; i < nx; i++) {
+    x = xL + i * dx;
+    // #pragma omp parallel for schedule(dynamic)
+    for (int j = r; j < r + 1; j++) {
+      y = yL + j * dy;
+      c = x + I * y;
+      matrix[i] = mandelbrot_func(0 * I, c, 0, Imax);
+    }
+  }
+
+  return matrix;
+}
+
+// mb_t* mandelbrot_matrix(int nx, int ny, double xL, double yL, double xR, double yR, int Imax)
+// {
+//   mb_t* M;
+//   mb_t* M_whole;
+//   mb_t* MT;
+//   int rank, size, dw, dh;
+//   int* recvcounts;
+//   int* displs;
+//   double dx, dy;
+
+//   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//   MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+//   dx = (double) (xR - xL) / (double) size;
+//   dy = (double) (yR - yL) / (double) size;
+//   dw = (int) floor((double) nx / (double) size);
+//   dh = (int) floor((double) ny / (double) size);
+  
+//   printf("dx = %.5f, rank = %d, dh = %d, size = %d\n", dx, rank, dh, size);
+//   M = _mandelbrot_matrix(
+//     nx,
+//     rank == size - 1 ? dh + (ny % size) : dh,
+//     xL,
+//     yL + rank * dy,
+//     xR,
+//     rank == size - 1 ? yR : yL + (rank + 1) * dy,
+//     Imax
+//   );
+//   // M = _mandelbrot_matrix(
+//   //   rank == size - 1 ? dw + (nx % size) : dw,
+//   //   ny,
+//   //   xL + rank * dx,
+//   //   yL,
+//   //   rank == size - 1 ? xR : xL + (rank + 1) * dx,
+//   //   yR,
+//   //   Imax
+//   // );
+//   if (rank == 0) {
+//     M_whole = (mb_t*) malloc(nx * ny * sizeof(mb_t));
+//     recvcounts = (int*) malloc(sizeof(int) * size);
+//     displs = (int*) malloc(sizeof(int) * size);
+//     for (int r = 0; r < size; r++) {
+//       recvcounts[r] = ((r == size - 1) ? dh + (ny % size) : dh) * nx;
+//       displs[r] = r == 0 ? 0 : displs[r - 1] + recvcounts[r - 1];
+//       printf("recvcounts[%d] = %d, displs[%d] = %d\n", r, recvcounts[r], r, displs[r]);
+//     }
+//   }
+//   // if (rank == 0) {
+//   //   M_whole = (mb_t*) malloc(nx * ny * sizeof(mb_t));
+//   //   recvcounts = (int*) malloc(sizeof(int) * size);
+//   //   displs = (int*) malloc(sizeof(int) * size);
+//   //   for (int r = 0; r < size; r++) {
+//   //     recvcounts[r] = ((r == size - 1) ? dw + (nx % size) : dw) * ny;
+//   //     displs[r] = r == 0 ? 0 : displs[r - 1] + recvcounts[r - 1];
+//   //     printf("recvcounts[%d] = %d, displs[%d] = %d\n", r, recvcounts[r], r, displs[r]);
+//   //   }
+//   // }
+//   printf("dw * ny = %d\n", dw * ny);
+
+//   // MPI_Gatherv(M, (rank == size - 1 ? (dh + (ny % size)) : dh) * nx, MPI_UNSIGNED_SHORT, M_whole, recvcounts, displs, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+//   MPI_Gatherv(M, (rank == size - 1 ? (dw + (nx % size)) : dw) * ny, MPI_UNSIGNED_SHORT, M_whole, recvcounts, displs, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+
+//   // MT = (mb_t*) malloc(nx * ny * sizeof(mb_t));
+//   // for (int i = 0; i < nx; i++) {
+//   //   for (int j = 0; j < ny; j++) {
+//   //     MT[i * ny + j] = M_whole[j * nx + i];
+//   //   }
+//   // }
+
+//   if (rank == 0) {
+//     // for (int i = 0; i < size; i++) {
+//     //   printf("rank=%d M[0] = %d\n", i, M_whole[displs[i]]);
+//     //   printf("rank=%d M[1] = %d\n", i, M_whole[displs[i] + 1]);
+//     // }
+//     free(recvcounts);
+//     free(displs);
+//   }
+  
+//   return M;
+// }
+
+mb_t* mandelbrot_matrix_rr(int nx, int ny, double xL, double yL, double xR, double yR, int Imax)
+{
+  int size, rank, done = 0;
+  int resolved_request_idx;
+  MPI_Status status;
+  MPI_Request* recv_requests;
   mb_t* M;
-  int rank, size, dw, dh;
-  int* recvcounts;
-  int* displs;
-  double dx, dy;
+  struct timeval start, end;
+  long microseconds = -1;
 
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  // dx = (double) (xR - xL) / (double) (size - 1);
-  // dw = (int) floor((double) nx / (double) (size - 1));
-  // printf("dx = %.5f, rank = %d, dw = %d, size = %d\n", dx, rank, dw, size);
-  // if (rank > 0) {
-  //   M = _mandelbrot_matrix(
-  //     rank == size - 1 ? dw + nx % (size - 1) : dw,
-  //     ny, 
-  //     xL + (rank - 1) * dx,
-  //     yL, 
-  //     rank == size - 1 ? xR : xL + rank * dx,
-  //     yR,
-  //     Imax
-  //   );
-  // } else {
-  //   M = (mb_t*) malloc(nx * ny * sizeof(mb_t));
-  //   recvcounts = (int*) malloc(sizeof(int) * size);
-  //   displs = (int*) malloc(sizeof(int) * size);
-  //   recvcounts[0] = 0;
-  //   displs[0] = 0;
-  //   for (int r = 1; r < size; r++) {
-  //     recvcounts[r] = r == size - 1 ? dw + nx % (size - 1) : dw;
-  //     printf("recvcounts[%d] = %d\n", r, recvcounts[r]);
-  //     displs[r] = displs[r - 1] + recvcounts[r - 1];
-  //   }
-  // }
-  // printf("dw * ny = %d\n", dw * ny);
+  if (rank > 0) {
+    // signal root that this rank is available to receive a new task to carry out
+    M = (mb_t*) malloc(nx * sizeof(mb_t));
+    MPI_Send(M, nx, MPI_UNSIGNED_SHORT, 0, TAG_TASK_ROW_RESULT, MPI_COMM_WORLD);
+    printf("[rank %d] sent dummy result\n", rank);
 
+    while (!done) {
+      recv_requests = (MPI_Request*) malloc(sizeof(MPI_Request) * 2);
+      int requested_row;
 
-  // MPI_Gatherv(M, rank == 0 ? 0 : ((rank == size - 1 ? dw + nx % (size - 1) : dw)) * ny, MPI_UNSIGNED_SHORT, M, recvcounts, displs, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
-  // // MPI_Gather(M, (rank == size - 1 ? dw + nx % (size - 1) : dw) * ny, MPI_UNSIGNED_SHORT, M, (rank == size - 1 ? dw + nx % (size - 1) : dw), MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+      // setup receival of the completion message, sent by root when all due work is done
+      MPI_Irecv(&done, 1, MPI_INT, 0, TAG_BCAST_DONE, MPI_COMM_WORLD, recv_requests);
 
-  dx = (double) (xR - xL) / (double) size;
-  dy = (double) (yR - yL) / (double) size;
-  dw = (int) floor((double) nx / (double) size);
-  dh = (int) floor((double) ny / (double) size);
-  
-  printf("dx = %.5f, rank = %d, dh = %d, size = %d\n", dx, rank, dh, size);
-  M = _mandelbrot_matrix(
-    // rank == size - 1 ? dw + (nx % size) : dw,
-    nx,
-    // ny,
-    rank == size - 1 ? dh + (ny % size) : dh,
-    // xL + rank * dx,
-    xL,
-    // yL,
-    yL + rank * dy,
-    // rank == size - 1 ? xR : xL + (rank + 1) * dx,
-    xR,
-    // yR,
-    rank == size - 1 ? yR : yL + (rank + 1) * dy,
-    Imax
-  );
-  if (rank == 0) {
-    M = (mb_t*) malloc(nx * ny * sizeof(mb_t));
-    recvcounts = (int*) malloc(sizeof(int) * size);
-    displs = (int*) malloc(sizeof(int) * size);
-    for (int r = 0; r < size; r++) {
-      // recvcounts[r] = (r == size - 1 ? dw + (nx % size) : dw) * ny;
-      recvcounts[r] = (r == size - 1 ? dh + (ny % size) : dh) * nx;
-      displs[r] = r == 0 ? 0 : displs[r - 1] + recvcounts[r - 1];
-      printf("recvcounts[%d] = %d, displs[%d] = %d\n", r, recvcounts[r], r, displs[r]);
-    }
-  }
-  printf("dw * ny = %d\n", dw * ny);
+      // setup receival of a task to be performed
+      MPI_Irecv(&requested_row, 1, MPI_INT, 0, TAG_TASK_ROW, MPI_COMM_WORLD, recv_requests + 1);
 
+      // printf("[rank %d] entering barrier\n", rank);
+      // MPI_Barrier(MPI_COMM_WORLD);
 
-  // MPI_Gatherv(M, (rank == size - 1 ? dw + (nx % size) : dw) * ny, MPI_UNSIGNED_SHORT, M, recvcounts, displs, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
-  MPI_Gatherv(M, (rank == size - 1 ? dh + (ny % size) : dh) * nx, MPI_UNSIGNED_SHORT, M, recvcounts, displs, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
-  // MPI_Gather(M, (rank == size - 1 ? dw + nx % (size - 1) : dw) * ny, MPI_UNSIGNED_SHORT, M, (rank == size - 1 ? dw + nx % (size - 1) : dw), MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+      // wait for either the completion message or a new task
+      printf("[rank %d] waiting for either a task or completion\n", rank);
+      MPI_Waitany(2, recv_requests, &resolved_request_idx, &status);
 
-  if (rank == 0) {
-    printf("M = ");
-    for (int i = 0; i < nx; i++) {
-      for (int j = 0; j < ny; j++) {
-        printf("%d ", M[j * nx + i]);
+      gettimeofday(&end, NULL);
+      if (microseconds == -1) {
+        microseconds = 0;
+      } else {
+        microseconds += end.tv_usec - start.tv_usec;
+        printf("[rank %d] waiting time: %06ld microseconds\n", rank, microseconds);
       }
-      printf("\n");
+
+      if (status.MPI_TAG == TAG_TASK_ROW) {
+        printf("[rank %d] computing row %d\n", rank, requested_row);
+        // perform the actual computation of the requested row
+        M = _mandelbrot_matrix_row(
+          requested_row,
+          nx,
+          ny,
+          xL,
+          yL,
+          xR,
+          yR,
+          Imax
+        );
+        // send the result back to root
+        MPI_Send(M, nx, MPI_UNSIGNED_SHORT, 0, TAG_TASK_ROW_RESULT, MPI_COMM_WORLD);
+
+        gettimeofday(&start, NULL);
+      }
+      free(recv_requests);
     }
-    free(recvcounts);
-    free(displs);
+  } else {
+    int nrow, next_row;
+    int* assigned_rows;
+    MPI_Status status;
+    mb_t* row;
+
+    assigned_rows = (int*) malloc(sizeof(int) * size);
+    for (int i = 0; i < size; i++) {
+      assigned_rows[i] = -1;
+    }
+
+    next_row = 0;
+    M = (mb_t*) malloc(sizeof(mb_t) * nx * ny);
+    row = (mb_t*) malloc(sizeof(mb_t) * nx);
+    while (next_row < ny) {
+      // wait for any rank to be ready to receive a new task (row to be computed)
+      MPI_Recv(row, nx, MPI_UNSIGNED_SHORT, MPI_ANY_SOURCE, TAG_TASK_ROW_RESULT, MPI_COMM_WORLD, &status);
+
+      nrow = assigned_rows[status.MPI_SOURCE];
+      if (nrow != -1) {
+        memcpy(M + nrow * nx, row, nx * sizeof(mb_t));
+      }
+
+      // printf("[rank %d] entering barrier\n", rank);
+      // MPI_Barrier(MPI_COMM_WORLD);
+
+      // send the ready rank some work to do, i.e. the next available row to be computed
+      printf("assigning row %d to rank %d\n", next_row, status.MPI_SOURCE);
+      MPI_Send(&next_row, 1, MPI_INT, status.MPI_SOURCE, TAG_TASK_ROW, MPI_COMM_WORLD);
+      assigned_rows[status.MPI_SOURCE] = next_row;
+      next_row++;
+    }
+
+    MPI_Request* done_requests = (MPI_Request*) malloc(sizeof(MPI_Request) * size);
+    for (int i = 0; i < size; i++) {
+      MPI_Isend(&next_row, 1, MPI_INT, i, TAG_BCAST_DONE, MPI_COMM_WORLD, done_requests + i);
+    }
+    MPI_Waitall(size, done_requests, MPI_STATUSES_IGNORE);
   }
-  
+  printf("[rank %d] total waiting time: %06ld microseconds\n", rank, microseconds);
   return M;
 }
 
@@ -210,17 +348,18 @@ int main(int argc, char** argv)
     exit(1);
   }
 
-  if (size > 1) {
-    M = mandelbrot_matrix(nx, ny, xL, yL, xR, yR, Imax);
-  } else {
-    M = _mandelbrot_matrix(nx, ny, xL, yL, xR, yR, Imax);
-  }
+  // if (size > 1) {
+  //   M = mandelbrot_matrix(nx, ny, xL, yL, xR, yR, Imax);
+  // } else {
+  //   M = _mandelbrot_matrix(nx, ny, xL, yL, xR, yR, Imax);
+  // }
   // for (int i = 0; i < nx; i++) {
   //   for (int j = 0; j < ny; j++) {
   //     printf("%d ", M[i * ny + j]);
   //   }
   //   printf("\n");
   // }
+  M = mandelbrot_matrix_rr(nx, ny, xL, yL, xR, yR, Imax);
 
   if (rank == 0) {
     write_pgm_image(M, Imax, nx, ny, image_name);
@@ -240,7 +379,6 @@ int main(int argc, char** argv)
   //   printf("I am thread %d on rank %d\n", thread_id, rank);
   // }
 
-  free(M);
   MPI_Finalize(); 
   return 0; 
 }
